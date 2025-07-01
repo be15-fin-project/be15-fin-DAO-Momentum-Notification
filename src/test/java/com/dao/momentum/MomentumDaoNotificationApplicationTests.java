dec49772113b549b27b01c38ca447505e260d80a
splguyjr <splguyjr@naver.com>
2025-07-01 09:30:38 +0900

    [init] 프로젝트 생성 및 초기 설정

---
+++ src/test/java/com/dao/momentum/MomentumDaoNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dao.momentum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentumDaoNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
